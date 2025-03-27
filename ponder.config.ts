1917de43da444336e2d50531bde45c9da2dc6773
gdotcat <noobmdev@gmail.com>
2025-03-27 19:07:31 +0700

    chore: temp comment mainnet

--- ponder.config.ts
+++ ponder.config.ts
@@ -5,20 +5,20 @@ import { erc20ABI } from "./abis/erc20ABI";
 export default createConfig({
 	ordering: "multichain",
 	networks: {
-		mainnet: {
-			chainId: 1,
-			transport: loadBalance([
-				...(process.env.PONDER_RPC_URL_1?.split(",")
-					.filter((el) => !!el)
-					.map((rpc) => http(rpc) as Transport) || []),
-				...(process.env.PONDER_WSS_URL_1?.split(",")
-					.filter((el) => !!el)
-					.map((ws) => webSocket(ws)) || []),
-				...(process.env.PONDER_RATELIMIT_RPC_URL_1?.split(",")
-					.filter((el) => !!el)
-					.map((rpc) => rateLimit(http(rpc), { requestsPerSecond: 1 })) || []),
-			]),
-		},
+		// mainnet: {
+		// 	chainId: 1,
+		// 	transport: loadBalance([
+		// 		...(process.env.PONDER_RPC_URL_1?.split(",")
+		// 			.filter((el) => !!el)
+		// 			.map((rpc) => http(rpc) as Transport) || []),
+		// 		...(process.env.PONDER_WSS_URL_1?.split(",")
+		// 			.filter((el) => !!el)
+		// 			.map((ws) => webSocket(ws)) || []),
+		// 		...(process.env.PONDER_RATELIMIT_RPC_URL_1?.split(",")
+		// 			.filter((el) => !!el)
+		// 			.map((rpc) => rateLimit(http(rpc), { requestsPerSecond: 1 })) || []),
+		// 	]),
+		// },
 		sepolia: {
 			chainId: 11155111,
 			transport: loadBalance([
@@ -52,13 +52,13 @@ export default createConfig({
 		ERC20: {
 			abi: erc20ABI,
 			network: {
-				mainnet: {
-					address: [
-						"0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
-						"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
-					],
-					startBlock: 22133577,
-				},
+				// mainnet: {
+				// 	address: [
+				// 		"0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
+				// 		"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
+				// 	],
+				// 	startBlock: 22133577,
+				// },
 				sepolia: {
 					address: ["0xf0e5aad1cbf9cd06ae87c4b7a6ed7dca3ad3e060"],
 					startBlock: 7985396,
